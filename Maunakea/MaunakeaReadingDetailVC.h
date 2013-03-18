da4bdb93d8aa36761bfe8a7991020bfa77b5d8b4
Darshan Desai <darshan.desai@intel.com>
2013-03-17 21:43:04 -0700

    Added second ReadingDetailVC - hasnt enabled it yet

---
+++ Maunakea/MaunakeaReadingDetailVC.h
@@ -0,0 +1,18 @@
+//
+//  MaunakeaReadingDetailVC.h
+//  Maunakea
+//
+//  Created by Darshan Desai on 3/17/13.
+//  Copyright (c) 2013 Darshan Desai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MaunakeaReadingDetailVC : UIViewController <UISplitViewControllerDelegate>
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+
+@end
